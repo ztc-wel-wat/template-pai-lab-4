d85a1b630fb2e29c06d22c944d364ade97f9a080
Łukasz Drozd <lukasz.drozd1@student.wat.edu.pl>
2021-06-15 13:29:46 +0200

    Zadania 4.7 - prezentacja szczegółowych danych książki

--- templates/mainTemplate.php
+++ templates/mainTemplate.php
@@ -3,77 +3,78 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>Portal</title>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Portal</title>
 </head>
 
 <body>
 
-    <div id="topDiv" class="scafoldingDiv">
-        <div id="headerMainDiv">
-            <a href="index.php">Główna</a> |
-            <a href="index.php?action=showSearchForm">Szukaj książki</a> |
-            <a href="index.php?action=showBasket">Twój koszyk</a> |
-            <a href="index.php?action=showRegistrationForm">Rejestracja</a>
-        </div>
-        <div id="headerUserInfoDiv">
-            <?php if ($portal->zalogowany) : ?>
-                <div>Jesteś zalogowany jako: <?= $portal->zalogowany->nazwa ?></div>
-                <div><a href="index.php?action=logout">Wylogowanie</a></div>
-            <?php else : ?>
-                <div>Nie jesteś zalogowany.</div>
-                <div><a href="index.php?action=showLoginForm">Logowanie</a></div>
-            <?php endif ?>
-        </div>
+  <div id="topDiv" class="scafoldingDiv">
+    <div id="headerMainDiv">
+      <a href="index.php">Główna</a> |
+      <a href="index.php?action=showSearchForm">Szukaj książki</a> |
+      <a href="index.php?action=showBasket">Twój koszyk</a> |
+      <a href="index.php?action=showRegistrationForm">Rejestracja</a>
     </div>
-
-    <div id="centerDiv" class="scafoldingDiv">
-        <div id="mainContentDiv">
-
-            <?php if ($komunikat) : ?>
-                <div class="komunikat"><?= $komunikat; ?></div>
-            <?php endif; ?>
-
-            <?php
-            switch ($action):
-                case 'showLoginForm':
-                    //Wyświetlenie formularza logowania
-                    include('templates/loginForm.php');
-                    break;
-                case 'showRegistrationForm':
-                    //Wyświetlenie formularza wyszukiwania
-                    $portal->showRegistrationForm();
-                    break;
-                case 'showSearchForm':
-                    //Wyświetlenie formularza rejestracyjnego
-                    $portal->showSearchForm();
-                    break;
-                case 'searchBook':
-                    //Wyszukanie książki
-                    $portal->showSearchForm();
-                    $portal->showSearchResults();
-                    break;
-                case 'showBookDetails':
-                    //Wyświetlenie szczegółowych informacji o książce
-                    break;
-                case 'showBasket':
-                    //Wyświetlenie zawartości koszyka
-                    break;
-                case 'checkout':
-                    //Wyświetlenie podsumowania zamówienia
-                    break;
-                case 'showMain':
-                default:
-                    include 'templates/innerContentDiv.php';
-            endswitch;
-            ?>
-        </div>
+    <div id="headerUserInfoDiv">
+      <?php if ($portal->zalogowany) : ?>
+        <div>Jesteś zalogowany jako: <?= $portal->zalogowany->nazwa ?></div>
+        <div><a href="index.php?action=logout">Wylogowanie</a></div>
+      <?php else : ?>
+        <div>Nie jesteś zalogowany.</div>
+        <div><a href="index.php?action=showLoginForm">Logowanie</a></div>
+      <?php endif ?>
     </div>
+  </div>
 
-    <div id="footerDiv" class="scafoldingDiv">
-        <p>Stopka strony</p>
+  <div id="centerDiv" class="scafoldingDiv">
+    <div id="mainContentDiv">
+
+      <?php if ($komunikat) : ?>
+        <div class="komunikat"><?= $komunikat; ?></div>
+      <?php endif; ?>
+
+      <?php
+      switch ($action):
+        case 'showLoginForm':
+          //Wyświetlenie formularza logowania
+          include('templates/loginForm.php');
+          break;
+        case 'showRegistrationForm':
+          //Wyświetlenie formularza wyszukiwania
+          $portal->showRegistrationForm();
+          break;
+        case 'showSearchForm':
+          //Wyświetlenie formularza rejestracyjnego
+          $portal->showSearchForm();
+          break;
+        case 'searchBook':
+          //Wyszukanie książki
+          $portal->showSearchForm();
+          $portal->showSearchResults();
+          break;
+        case 'showBookDetails':
+          //Wyświetlenie szczegółowych informacji o książce
+          $portal->showBookDetails();
+          break;
+        case 'showBasket':
+          //Wyświetlenie zawartości koszyka
+          break;
+        case 'checkout':
+          //Wyświetlenie podsumowania zamówienia
+          break;
+        case 'showMain':
+        default:
+          include 'templates/innerContentDiv.php';
+      endswitch;
+      ?>
     </div>
+  </div>
+
+  <div id="footerDiv" class="scafoldingDiv">
+    <p>Stopka strony</p>
+  </div>
 
 </body>
 
