1948a2a2dbff1b6bcf545b7cc7679ef5e05abe17
Emilia Spanehl <emilia.spanehl@student.wat.edu.pl>
2021-06-15 13:03:12 +0200

    Zadania 4.3 – logowanie i wylogowanie

---
+++ klasy/user.php
@@ -0,0 +1,21 @@
+<?php
+
+class USER
+
+{
+
+    public $id;
+
+    public $nazwa;
+
+    function __construct($id, $nazwa)
+
+    {
+
+        $this->id = $id;
+
+        $this->nazwa = $nazwa;
+
+    }
+
+}
--- support/query.sql
+++ support/query.sql
@@ -404,3 +404,12 @@ VALUES (1, 1, 1, 29.90),
     
 INSERT INTO AutorzyPseudonimy
 VALUES (5, 2);
+
+
+ALTER TABLE Klienci ADD COLUMN `Email` VARCHAR(245) UNIQUE;
+ALTER TABLE Klienci ADD COLUMN `Haslo` VARCHAR(100) NOT NULL;
+ALTER TABLE Klienci CHANGE COLUMN `Nr domu` `Nr_domu` VARCHAR(5) NOT NULL;
+ALTER TABLE Klienci CHANGE COLUMN `Nr mieszkania` `Nr_mieszkania` VARCHAR(5);
+ALTER TABLE Klienci CHANGE COLUMN `Miasto` `Miejscowosc` VARCHAR(60) NOT NULL;
+
+SELECT `Id`, `Imie`, `Nazwisko`, `Haslo` FROM Kliecni WHERE `Email`='$email'
