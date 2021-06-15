e3a3939dd43384a73dace4ef5bc40f87e0131b94
Rafal Palaszewski <rafal.palaszewski@student.wat.edu.pl>
2021-06-15 14:15:55 +0200

    Zadanie 4.5 – usprawnianie procedury rejestracji

--- klasy/Registration.php
+++ klasy/Registration.php
@@ -20,48 +20,71 @@ class Registration{
     $this->fields['kod'] = new FormInput('kod', 'Kod pocztowy');
     $this->fields['kraj'] = new FormInput('kraj', 'Kraj');
    }
- public function showRegistrationForm() {
+   public function showRegistrationForm()
+   {
+    foreach ($this->fields as $name => $field)
+    $field->value = isset($_SESSION['formData'][$name]) ? $_SESSION['formData'][$name] : '';
     $formData = $this->fields;
+    if (isset($_SESSION['formData']))
+    unset($_SESSION['formData']);
     include 'templates/registrationForm.php';
    }
    
-   public function registerUser() {
-    // Sprawdzenie czy wszystkie pola ustawione
-    foreach ($this->fields as $name => $val) {
-    if (!isset($_POST[$name]))
-    return FORM_DATA_MISSING;
-    }
-    // Odczyt i przefiltrowanie danych z formularza
-    $fieldsFromForm = array();
-    $emptyFieldDetected = false;
-    foreach ($this->fields as $name => $val) {
-    if ($val->type != 'password')
-    $fieldsFromForm[$name] = filter_input(INPUT_POST, $name, FILTER_SANITIZE_SPECIAL_CHARS);
-    else
-    $fieldsFromForm[$name] = $_POST[$name];
-    $fieldsFromForm[$name] = $this->dbo->real_escape_string($fieldsFromForm[$name]);
-    if ($fieldsFromForm[$name] == '' && $val->required)
-    $emptyFieldDetected = true;
-    }
-    // Sprawdzenie, czy wykryto puste pola
-    if ($emptyFieldDetected == true)
-    return FORM_DATA_MISSING;
-    // Sprawdzenie, czy podany e-mail jest już w bazie
-    $query = "SELECT COUNT(*) FROM klienci WHERE Email=" . $fieldsFromForm['email'] . "'";
-    if ($this->dbo->getQuerySingleResult($query) > 0)
-    return USER_NAME_ALREADY_EXISTS;
-    // Sprawdzenie zgodności hasła z obu pól
-    if ($fieldsFromForm['haslo'] != $fieldsFromForm['haslo2'])
-    return PASSWORDS_DO_NOT_MATCH;
-    unset($fieldsFromForm['haslo2']);
-    unset($this->fields['haslo2']);
-    // Przygotowanie ciągów nazw pól i wartości pól dla zapytania SQL
-    $fieldsName = '`' . implode('`,`', array_keys($this->fields)) . '`';
-    $fieldsVals = '\'' . implode('\',\'', $fieldsFromForm) . '\'';
-    $query = "INSERT INTO Klienci ($fieldsName) VALUES ($fieldsVals)";
-    if ($this->dbo->query($query))
-    return ACTION_OK;
-    else
-    return ACTION_FAILED;
+   
+   public function registerUser()
+   {
+       // Sprawdzenie czy wszystkie pola ustawione
+       foreach ($this->fields as $name => $val) {
+           if (!isset($_POST[$name]))
+               return FORM_DATA_MISSING;
+       }
+       // Odczyt i przefiltrowanie danych z formularza
+       $fieldsFromForm = array();
+       $emptyFieldDetected = false;
+       foreach ($this->fields as $name => $val) {
+           if ($val->type != 'password')
+               $fieldsFromForm[$name] = filter_input(INPUT_POST, $name, FILTER_SANITIZE_SPECIAL_CHARS);
+           else
+               $fieldsFromForm[$name] = $_POST[$name];
+           $fieldsFromForm[$name] = $this->dbo->real_escape_string($fieldsFromForm[$name]);
+           if ($fieldsFromForm[$name] == '' && $val->required)
+               $emptyFieldDetected = true;
+       }
+
+       // Sprawdzenie, czy wykryto puste pola
+       if ($emptyFieldDetected == true) {
+           unset($fieldsFromForm['haslo']);
+           unset($fieldsFromForm['haslo2']);
+           $_SESSION['formData'] = $fieldsFromForm;
+           return FORM_DATA_MISSING;
+       }
+
+       // Sprawdzenie, czy podany e-mail jest już w bazie
+       $query = "SELECT COUNT(*) FROM klienci WHERE Email=" . $fieldsFromForm['email'] . "'";
+       if ($this->dbo->getQuerySingleResult($query) > 0) {
+           unset($fieldsFromForm['haslo']);
+           unset($fieldsFromForm['haslo2']);
+           $_SESSION['formData'] = $fieldsFromForm;
+           return USER_NAME_ALREADY_EXISTS;
+       }
+
+       // Sprawdzenie zgodności hasła z obu pól
+       if ($fieldsFromForm['haslo'] != $fieldsFromForm['haslo2']) {
+           unset($fieldsFromForm['haslo']);
+           unset($fieldsFromForm['haslo2']);
+           $_SESSION['formData'] = $fieldsFromForm;
+           return PASSWORDS_DO_NOT_MATCH;
+       }
+
+       unset($fieldsFromForm['haslo2']);
+       unset($this->fields['haslo2']);
+       // Przygotowanie ciągów nazw pól i wartości pól dla zapytania SQL
+       $fieldsName = '`' . implode('`,`', array_keys($this->fields)) . '`';
+       $fieldsVals = '\'' . implode('\',\'', $fieldsFromForm) . '\'';
+       $query = "INSERT INTO Klienci ($fieldsName) VALUES ($fieldsVals)";
+       if ($this->dbo->query($query))
+           return ACTION_OK;
+       else
+           return ACTION_FAILED;
    }
 }
