0e93a3fc26c6fb7891a2a14de90ac875c06960b9
Lukasz-Osiadacz <lukasz.osiadacz@student.wat.edu.pl>
2021-06-15 13:10:55 +0200

    Zadania 4.3 – logowanie i wylogowanie

--- support/query.sql
+++ support/query.sql
@@ -74,3 +74,15 @@ ADD CONSTRAINT `KsiazkiZamowieniaZamowienieIdFK` FOREIGN KEY (`ZamowienieId`) RE
 Zamowienia(`Id`);
 ALTER TABLE KsiazkiZamowienia
 ADD CONSTRAINT `KsiazkiZamowieniaKsiazkaIdFK` FOREIGN KEY (`KsiazkaId`) REFERENCES Ksiazki(`Id`);
+
+ALTER TABLE Klienci ADD COLUMN `Email` VARCHAR(245) UNIQUE;
+ALTER TABLE Klienci ADD COLUMN `Haslo` VARCHAR(100) NOT NULL;
+
+ALTER TABLE Klienci CHANGE COLUMN `Nr domu` `Nr_domu` VARCHAR(5) NOT NULL;
+ALTER TABLE Klienci CHANGE COLUMN `Nr mieszkania` `Nr_mieszkania` VARCHAR(5);
+
+ALTER TABLE Klienci CHANGE COLUMN `Miasto` `Miejscowosc` VARCHAR(60) NOT NULL;
+
+SELECT `Id`, `Imie`, `Nazwisko`, `Haslo` FROM Kliecni WHERE `Email`='$email'
+
+UPDATE klienci SET Email = "wat@gmail.com", Haslo = "Wat1234" WHERE Id = 1
